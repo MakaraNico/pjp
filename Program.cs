06ffe9f0220307940c476e360fca21c580fbccc5
Nico <nico.makara50@gmail.com>
2024-02-20 12:22:24 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace pjp_cv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines;
+            int lines_num = ParsingHandler.ParseNumLines();
+            if (lines_num == 0)
+            {
+                return;
+            }
+            lines = ParsingHandler.ReadLines(lines_num);
+            lines = ParsingHandler.RemoveSpaces(lines);
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                int res = ParsingHandler.SolveLine(lines[i]);
+                if (res == 0)
+                {
+                    Console.WriteLine("ERROR");
+                    continue;
+                }
+
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
